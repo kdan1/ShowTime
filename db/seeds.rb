628531128a808c1c0e97a062b4ee53260292a940
kdan1 <kathydan96@gmail.com>
2016-09-03 18:08:28 -0700

    added an admin attribute to users, created an admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(:email => "admin1@example.com",  :password => "admin1", :password_confirmation => "admin1", admin: true)
